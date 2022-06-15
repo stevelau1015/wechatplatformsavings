f15846175b55ef93c8bdab7d20cb2a6ec24ec053
stevelau1015 <1463089520@qq.com>
2022-06-15 15:15:41 +0800

    存档公众号

---
+++ loggerr.py
@@ -0,0 +1,17 @@
+import sys
+
+
+
+# 控制台输出记录到文件
+class Logger(object):
+    def __init__(self, file_name="Default.log", stream=sys.stdout):
+        self.terminal = stream
+        self.log = open(file_name, "a")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+    def flush(self):
+        pass
+
